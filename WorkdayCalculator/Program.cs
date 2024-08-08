5ae8a325d77f6f92e75526010c00d22f8a9106db
Junayet8008 <jkandoker8008@gmail.com>
2024-08-08 14:50:16 -0400

    complete

---
+++ WorkdayCalculator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkdayCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkdayCalculator calendar = new WorkdayCalculator();
+            calendar.SetWorkdayStartAndStop(8, 0, 16, 0);
+            calendar.SetRecurringHoliday(5, 17);
+            calendar.SetHoliday(new DateTime(2004, 5, 27));
+
+            string format = "dd-MM-yyyy HH:mm";
+
+            // Test Case 1
+            var start1 = new DateTime(2004, 5, 24, 19, 3, 0);
+            decimal increment1 = 44.723656m;
+            var incrementedDate1 = calendar.GetWorkdayIncrement(start1, increment1);
+            Console.WriteLine(
+                start1.ToString(format) +
+                " with an addition of " +
+                increment1 +
+                " work days is " +
+                incrementedDate1.ToString(format));
+
+            // Test Case 2
+            var start2 = new DateTime(2004, 5, 24, 18, 3, 0);
+            decimal increment2 = -6.7470217m;
+            var incrementedDate2 = calendar.GetWorkdayIncrement(start2, increment2);
+            Console.WriteLine(
+                start2.ToString(format) +
+                " with an addition of " +
+                increment2 +
+                " work days is " +
+                incrementedDate2.ToString(format));
+
+            // Test Case 3
+            var start3 = new DateTime(2004, 5, 24, 8, 3, 0);
+            decimal increment3 = 12.782709m;
+            var incrementedDate3 = calendar.GetWorkdayIncrement(start3, increment3);
+            Console.WriteLine(
+                start3.ToString(format) +
+                " with an addition of " +
+                increment3 +
+                " work days is " +
+                incrementedDate3.ToString(format));
+
+            // Test Case 4
+            var start4 = new DateTime(2004, 5, 24, 7, 3, 0);
+            decimal increment4 = 8.276628m;
+            var incrementedDate4 = calendar.GetWorkdayIncrement(start4, increment4);
+            Console.WriteLine(
+                start4.ToString(format) +
+                " with an addition of " +
+                increment4 +
+                " work days is " +
+                incrementedDate4.ToString(format));
+            // Test Case 4
+            var start5 = new DateTime(2004, 5, 24, 18, 5, 0);
+            decimal increment = -5.5m;
+            var incrementedDate = calendar.GetWorkdayIncrement(start5, increment);
+            Console.WriteLine(
+            start5.ToString(format) +
+            " with an addition of " +
+            increment +
+            " work days is " +
+            incrementedDate.ToString(format));
+        }
+    }
+}
---
+++ WorkdayCalculator/WorkdayCalculator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkdayCalculator
+{
+    public class WorkdayCalculator : IWorkdayCalculator
+    {
+        private readonly HashSet<DateTime> _holidays = new HashSet<DateTime>();
+        private readonly List<(int month, int day)> _recurringHolidays = new List<(int, int)>();
+        private TimeSpan _workdayStart;
+        private TimeSpan _workdayEnd;
+
+        public void SetHoliday(DateTime date)
+        {
+            _holidays.Add(date.Date);
+        }
+
+        public void SetRecurringHoliday(int month, int day)
+        {
+            _recurringHolidays.Add((month, day));
+        }
+
+        public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
+        {
+            _workdayStart = new TimeSpan(startHours, startMinutes, 0);
+            _workdayEnd = new TimeSpan(stopHours, stopMinutes, 0);
+        }
+
+        public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
+        {
+            if (startDate.TimeOfDay > _workdayEnd)
+            {
+                startDate = new DateTime(startDate.Year,startDate.Month,startDate.Day,_workdayEnd.Hours,_workdayEnd.Minutes, 0);
+            }
+            if (startDate.TimeOfDay < _workdayStart)
+            {
+                startDate = PreviousWorkdayEnd(startDate);
+            }
+
+            TimeSpan workdayDuration = _workdayEnd - _workdayStart;
+
+            double totalMinutes = (double)incrementInWorkdays * workdayDuration.TotalMinutes;
+            DateTime resultDate;
+            if(totalMinutes == 0)
+            {
+                return startDate;
+            }
+            if (totalMinutes > 0)
+            {
+                resultDate = MoveForward(startDate, totalMinutes);
+            }
+            else
+            {
+                resultDate = MoveBackwards(startDate, -totalMinutes);
+            }
+            return resultDate;
+        }
+
+        private DateTime NextWorkdayStart(DateTime date)
+        {
+            DateTime nextDay = date.Date.AddDays(1);
+            while (!IsWorkday(nextDay))
+            {
+                nextDay = nextDay.AddDays(1);
+            }
+            return nextDay.Date + _workdayStart;
+        }
+
+        private DateTime PreviousWorkdayEnd(DateTime date)
+        {
+            DateTime prevDay = date.Date.AddDays(-1);
+            while (!IsWorkday(prevDay))
+            {
+                prevDay = prevDay.AddDays(-1);
+            }
+            return prevDay.Date + _workdayEnd;
+        }
+
+        private DateTime MoveForward(DateTime startDate, double totalMinutes)
+        {
+            DateTime currentDate = startDate;
+            while (totalMinutes > 0)
+            {
+                double remainingMinutesInDay = (_workdayEnd - currentDate.TimeOfDay).TotalMinutes;
+                if (totalMinutes <= remainingMinutesInDay)
+                {
+                    return currentDate.AddMinutes(totalMinutes);
+                }
+
+                totalMinutes -= remainingMinutesInDay;
+                currentDate = NextWorkdayStart(currentDate);
+            }
+            throw new InvalidOperationException("Unexpected state while moving forward.");
+        }
+
+        private DateTime MoveBackwards(DateTime startDate, double totalMinutes)
+        {
+            DateTime currentDate = startDate;
+            while (totalMinutes > 0)
+            {
+                double elapsedMinutesInDay = (currentDate.TimeOfDay - _workdayStart).TotalMinutes;
+                if (totalMinutes <= elapsedMinutesInDay)
+                {
+                    return currentDate.AddMinutes(-totalMinutes);
+                }
+
+                totalMinutes -= elapsedMinutesInDay;
+                currentDate = PreviousWorkdayEnd(currentDate);
+            }
+            throw new InvalidOperationException("Unexpected state while moving backwards.");
+        }
+
+        private bool IsWorkday(DateTime date)
+        {
+            if (_holidays.Contains(date.Date))
+            {
+                return false;
+            }
+
+            var recurringHoliday = _recurringHolidays.Find(h => h.month == date.Month && h.day == date.Day);
+            if (recurringHoliday != default)
+            {
+                return false;
+            }
+
+            // Check if it's a weekend
+            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+        }
+    }
+}
